be56be8b2949d24ee52b3809d2575454c1a71924
Suhaib Saiyde <suhaibpattazhi@gmail.com>
2024-10-01 10:56:57 +0530

    next commit

---
+++ MyMavenProject/src/main/java/Encapsulation/Encap2.java
@@ -0,0 +1,14 @@
+package Encapsulation;
+
+public class Encap2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Encap obj=new Encap();
+		obj.setName("hello");
+		obj.setAge(8);
+		System.out.println(obj.getAge());
+		System.out.println(obj.getName());
+	}
+
+}
